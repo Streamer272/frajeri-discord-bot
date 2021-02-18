7a8f98e8768640246a961d18d93bfd21a0c1ac69
Daniel Svitan <dankosaurus9@gmail.com>
2021-02-18 15:04:39 +0100

    :hammer: refactor code

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-import datetime
-import time
-import send
+from datetime import datetime, date
+from time import sleep
+import discord
 
-last_send_date = "wda"
+last_send_date = ""
+
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        channel = self.get_channel(809822474668736583)
+        await channel.send("Hey @everyone, you should go and pray in 5 minues...")
+        await self.close()
+
+def send():
+    client = CustomClient()
+    client.run("ODA5ODE2OTcxNTMyODI4Nzgy.YCam0w.cYBBRIwM2ZWnuEOoJrCfLj6nWaA")
+    print("Pray message send...")
 
 
 if __name__ == "__main__":
     print("Bot running...")
     while True:
         while True:
-            time_ = int(str(datetime.datetime.now().strftime("%H:%M:%S")).replace(":", ""))
-            if time_ > 140000 and last_send_date != str(datetime.date.today()):
-                last_send_date = str(datetime.date.today())
+            time_ = int(str(datetime.now().strftime("%H:%M:%S")).replace(":", ""))
+
+            if time_ > 135500 and last_send_date != str(date.today()):
+                last_send_date = str(date.today())
                 break
-            time.sleep(1)
-        send.send()
+
+            sleep(60)
+        send()
